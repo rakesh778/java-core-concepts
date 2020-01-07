fce60afb65399f7092fa60bafaeec3dd780ff330
rakesh <rakesh.amirishetty123@gmail.com>
2020-01-07 13:28:50 +0530

    not overriding run()

---
+++ JavaCoreConcepts/src/com/rakesh/Multithreading/NotOverridingRumMethod.java
@@ -0,0 +1,18 @@
+package com.rakesh.Multithreading;
+
+class Demo extends Thread {
+
+}
+
+public class NotOverridingRumMethod {
+
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		d.start();
+		System.out.println("main method");
+
+	}
+
+}
+//if we don't override run() inside child thread then parent(Thread class) run()
+// will be executed which is empty, so we don't get any output.
